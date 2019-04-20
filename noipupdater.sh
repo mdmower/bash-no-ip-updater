166e5ae37c13267ba5f5e7fccc1055e02a5763d0
kousu <nick@kousu.ca>
2019-04-19 23:36:02 -0400

    Remove code that #6 made dead

--- noipupdater.sh
+++ noipupdater.sh
@@ -62,25 +62,6 @@ cmd_exists() {
     command -v "$1" > /dev/null 2>&1
 }
 
-# IP Validator
-# http://www.linuxjournal.com/content/validating-ip-address-bash-script
-function valid_ip() {
-    local ip=$1
-    local stat=1
-
-    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        OIFS=$IFS
-        IFS='.'
-        ip=($ip)
-        IFS=$OIFS
-        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
-            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
-        stat=$?
-    fi
-
-    return $stat
-}
-
 function get_logline() {
     local host=$1
     local response=$(echo $2 | tr -cd "[:print:]")
