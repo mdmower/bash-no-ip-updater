ea042ad1029899ece02e68bbb0c06e193719bce7
Matt Mower <mowerm@gmail.com>
2019-04-27 11:43:13 -0700

    Revert "Remove code that #6 made dead"
    This reverts commit 4f9039cb19d3e6337f49cf01678a940ecad7515b.

--- noipupdater.sh
+++ noipupdater.sh
@@ -106,6 +106,25 @@ function http_get() {
     fi
 }
 
+# IP Validator
+# http://www.linuxjournal.com/content/validating-ip-address-bash-script
+function valid_ip() {
+    local ip=$1
+    local stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+
+    return $stat
+}
+
 function get_logline() {
     local host
     local response
